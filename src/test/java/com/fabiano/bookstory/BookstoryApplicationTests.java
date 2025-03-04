924ff37158920d4bbeb749317f220c37fb7931db
FabianoLAlmeida <fabianomarques0524@gmail.com>
2025-03-04 12:36:44 -0300

    commit incial

---
+++ src/test/java/com/fabiano/bookstory/BookstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiano.bookstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
