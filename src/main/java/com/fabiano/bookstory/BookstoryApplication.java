922138c5ff5a9bac6949ae23284985e55744434e
FabianoLAlmeida <fabianomarques0524@gmail.com>
2025-03-09 18:34:49 -0300

    instâncias de forma automatizadas

--- src/main/java/com/fabiano/bookstory/BookstoryApplication.java
+++ src/main/java/com/fabiano/bookstory/BookstoryApplication.java
@@ -1,13 +1,40 @@
 package com.fabiano.bookstory;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fabiano.bookstory.domain.Categoria;
+import com.fabiano.bookstory.domain.Livro;
+import com.fabiano.bookstory.repositories.CategoriaRepository;
+import com.fabiano.bookstory.repositories.LivroRepository;
+
 @SpringBootApplication
-public class BookstoryApplication {
+public class BookstoryApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoryApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+		
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Loren ipson", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+     		
 	}
 
 }
